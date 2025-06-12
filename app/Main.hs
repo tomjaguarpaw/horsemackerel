ac32f012333ec3f32efb8be546ca55259bbd1444
Tom Ellis <tom-git@jaguarpaw.co.uk>
2025-06-12 14:14:00 +0100

    Some parsing

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,9 @@ import Data.Aeson
 import Data.Aeson.Types (Parser, parseMaybe)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BL
-import System.IO (IOMode (WriteMode))
+import Data.List (nub, stripPrefix)
 import System.FilePath (takeDirectory)
+import System.IO (IOMode (WriteMode))
 import System.Process.Typed
 
 main :: IO ()
@@ -47,7 +48,7 @@ work io ystdout yfile = do
           yield yfile s
           let base = takeDirectory s
           (_exitCode, stdout, _stderr) <-
-              effIO io $ readProcess (proc "git" ["-C", base, "remote", "-v"])
+            effIO io $ readProcess (proc "git" ["-C", base, "remote", "-v"])
           yield yfile $ "Remotes:\n" ++ BS.unpack (BL.toStrict stdout)
 
   yield ystdout "Done"
@@ -55,3 +56,44 @@ work io ystdout yfile = do
 extractFilePath :: Value -> Parser String
 extractFilePath = withObject "root" $ \o ->
   o .: "payload" >>= withObject "payload" (\p -> p .: "file_path")
+
+parseRemotes :: String -> [String]
+-- FIXME: nub is O(n^2)
+parseRemotes s = nub $ do
+  flip map (lines s) $ \line ->
+    case words line of
+      [_remoteName, remoteUrl, _remoteType] -> remoteUrl
+      other -> error (show other)
+
+webPageOf :: String -> String -> String
+webPageOf gitUrl path = do
+  let (host, ':':gitFile) = break (== ':') gitUrl
+  case host of
+    "git@github.com" -> do
+      let basePath = case stripSuffix ".git" gitFile of
+            Just s -> s
+            Nothing -> error "Basepath"
+
+      "https://github.com/" <> basePath <> "/blob/master/" <> path
+    other -> error ("Unknown: " <> other)
+
+stripSuffix :: (Eq a) => [a] -> [a] -> Maybe [a]
+stripSuffix suf s = fmap reverse (stripPrefix (reverse suf) (reverse s))
+
+test :: Bool
+test = do
+  let remoteOutput =
+        unlines
+          [ "origin  git@github.com:tomjaguarpaw/ad.git (fetch)",
+            "origin  git@github.com:tomjaguarpaw/ad.git (push)"
+          ]
+
+  let expected = ["git@github.com:tomjaguarpaw/ad.git"]
+
+  let b1 = parseRemotes remoteOutput == expected
+
+  let b2 =
+        webPageOf "git@github.com:tomjaguarpaw/ad.git" "Term/app/Main.hs"
+          == "https://github.com/tomjaguarpaw/ad/blob/master/Term/app/Main.hs"
+
+  b1 && b2
