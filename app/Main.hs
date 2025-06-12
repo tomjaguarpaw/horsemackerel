1f99f12532249d1dfea60caa7bcbeff128d60e7f
Tom Ellis <tom-git@jaguarpaw.co.uk>
2025-06-12 13:06:38 +0100

    Works

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,10 @@ import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = do
+  putStrLn "horsemackerel Running..."
   input <- BS.getContents
+  putStrLn "Getting contents..."
   case decode (BL.fromStrict input) :: Maybe Value of
     Just v  -> writeFile "/tmp/horsemackerel.out" (show v)
     Nothing -> writeFile "/tmp/horsemackerel.out" "Invalid JSON"
+  putStrLn "Done"
--- elisp/horsemackerel.el
+++ elisp/horsemackerel.el
@@ -19,11 +19,10 @@
 
 (defun dump-buffer-info-to-fixed-file ()
   (interactive)
-  (let* ((json (generate-buffer-json))
-	 (process-name (format "emacs-out-process-%d"
-			       (setq emacs-out-process-counter (1+ emacs-out-process-counter)))))
-    (start-process process-name nil "/tmp/emacs-out.sh")
-    (process-send-string process-name json)
-    (process-send-eof process-name)))
+  (let ((process-connection-type nil)) ; use pipe
+    (let* ((json (generate-buffer-json))
+           (proc (start-process "emacs-out-process" "*emacs-out-log*" "/tmp/horsemackerel")))
+      (process-send-string proc json)
+      (process-send-eof proc))))
 
 (global-set-key (kbd "C-c j") 'dump-buffer-info-to-fixed-file)
